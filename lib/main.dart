2f4a2ff731cb1b32d9839723c670dc3f90c3e1d9
Alkonosttt <voodoomon@protonmail.com>
2025-05-10 23:43:31 +0200

    Implemented GNav

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,13 +15,27 @@ class MainApp extends StatelessWidget {
       title: 'Hypnos',
       theme: ThemeData(
         brightness: Brightness.dark,
-        //fontFamily: 'YourFontName',
         textTheme: const TextTheme(
           bodyMedium: TextStyle(color: Colors.white),
           displayLarge: TextStyle(color: Colors.white),
         ),
       ),
       home: Scaffold(
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+            tabBackgroundColor: Color(0xFF942F67),
+            gap: 10,
+            // TODO: add tab functionality
+            onTabChange: (index) {},
+            padding: EdgeInsets.all(8),
+            tabs: [
+              GButton(icon: Icons.music_note, text: 'library'),
+              GButton(icon: Icons.favorite, text: 'favourites'),
+              GButton(icon: Icons.settings, text: 'settings'),
+            ],
+          ),
+        ),
         body: Row(
           children: <Widget>[
             Expanded(flex: 1, child: SizedBox()),
