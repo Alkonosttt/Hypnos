e205e9538487952bc1176b0e3e674d675bed716a
Alkonosttt <voodoomon@protonmail.com>
2025-05-10 23:11:47 +0200

    Added dependencies for Google Nav Bar and Google Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,39 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      title: 'Hypnos',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        //fontFamily: 'YourFontName',
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.white),
+          displayLarge: TextStyle(color: Colors.white),
+        ),
+      ),
+      home: Scaffold(
+        body: Row(
+          children: <Widget>[
+            Expanded(flex: 1, child: SizedBox()),
+            Expanded(
+              flex: 8,
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 70, 0, 50),
+                    child: Text(
+                      'AMBIENT LIBRARY',
+                      style: GoogleFonts.instrumentSerif(fontSize: 45),
+                    ),
+                  ),
+                  Text('plaques', style: GoogleFonts.comfortaa(fontSize: 20)),
+                ],
+              ),
+            ),
+            Expanded(flex: 1, child: SizedBox()),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/savedcombinations.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SavedCombinations extends StatefulWidget {
+  const SavedCombinations({super.key});
+
+  @override
+  State<SavedCombinations> createState() => _SavedCombinationsState();
+}
+
+class _SavedCombinationsState extends State<SavedCombinations> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Saved combinations');
+  }
+}
