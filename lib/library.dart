373a20cb8238c504254a66166d5ba730bb962d61
Alkonosttt <voodoomon@protonmail.com>
2025-05-15 23:54:19 +0200

    Added a list of file names

--- lib/library.dart
+++ lib/library.dart
@@ -28,6 +28,30 @@ class LibraryPage extends StatefulWidget {
 }
 
 class _LibraryPageState extends State<LibraryPage> {
+  final List<String> soundNames = [
+    '40_hz_binaural',
+    'airplane_ambience',
+    'calming_rain',
+    'ceiling_fan_closeup_hum',
+    'cicada_buzzing',
+    'creek',
+    'cricket',
+    'fire_sound',
+    'forest_bird_harmonies',
+    'frogs_croaking',
+    'inside_old_train',
+    'light_rain',
+    'ocean_waves',
+    'pendulum_clock_tic_tac',
+    'soft_wind',
+    'swamp_woods',
+    'thunderstorm',
+    'ticking_clock',
+    'typing_on_laptop_keyboard',
+    'wind_blowing',
+  ];
+  late List<SoundItem> items;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
